3c81ac3ac80542cfc2ba0db0b684a0059625e11b
sazzad-anwar <sazzadzihan@gmail.com>
2022-01-23 00:22:20 +0600

    mobile responsive done

--- js/main.js
+++ js/main.js
@@ -63,3 +63,80 @@ $('.brands__carousel').owlCarousel({
         }
     }
 });
+
+let mobileFunctionality = () => {
+    console.log('mobile device')
+
+    // function while click on search icon
+    document.querySelector('.__search').addEventListener('click', () => {
+        document.querySelector(".mobile__search__result").classList.add('show__drawer')
+        document.querySelector(".mobile__search__result").classList.add('animate__fadeInLeft')
+
+        document.querySelector('.search__elements').removeAttribute('hidden');
+
+        document.querySelector('.search__heading h1').innerHTML = 'Search'
+
+        if (document.querySelector('.mobile__search__result').classList.contains('animate__fadeOutLeftBig')) {
+            document.querySelector(".mobile__search__result").classList.remove('animate__fadeOutLeftBig')
+        }
+
+        let searchHeadingHeight = document.querySelector('.search__heading').offsetHeight;
+        let searchInputgHeight = document.querySelector('.__search__input').offsetHeight;
+        let searchHeadHeight = document.querySelector('.recently__searched h1').offsetHeight;
+
+        document.querySelector('.recently__searched .overflow-auto').style.maxHeight = `calc(100vh - ${searchHeadingHeight + searchInputgHeight + searchHeadHeight}px)`
+    })
+
+    // function while click on close menu icon
+    document.querySelector('.close__drawer').addEventListener('click', () => {
+        document.querySelector(".mobile__search__result").classList.remove('animate__fadeInLeft')
+        document.querySelector(".mobile__search__result").classList.add('animate__fadeOutLeftBig')
+        document.querySelector(".mobile__search__result").classList.remove('show__drawer')
+    })
+
+    // function while click on menu icon
+    document.querySelector('.__menu').addEventListener('click', () => {
+        let searchHeadingHeight = document.querySelector('.search__heading').offsetHeight;
+        document.querySelector(".mobile__search__result").classList.add('show__drawer')
+        document.querySelector(".mobile__search__result").classList.add('animate__fadeInLeft')
+
+        document.querySelector('.menu__elements').removeAttribute('hidden');
+
+        if (!document.querySelector('.search__elements').getAttribute('hidden')) {
+            document.querySelector('.search__elements').setAttribute('hidden', true)
+        }
+
+        document.querySelector('.search__heading h1').innerHTML = 'Menu'
+
+        let menuList = document.querySelectorAll('.main__nav div ul li')
+        let regularNav = document.querySelectorAll('.bottom__nav .regular__nav li')
+        let topNav = document.querySelector('.regular__nav.nav__top');
+
+        menuList.forEach(menu => {
+            document.querySelector('.menu__elements ul').append(menu)
+        })
+
+        regularNav.forEach(menu => {
+            document.querySelector('.menu__elements ul').append(menu)
+        })
+
+        if (document.querySelector('.mobile__search__result').classList.contains('animate__fadeOutLeftBig')) {
+            document.querySelector(".mobile__search__result").classList.remove('animate__fadeOutLeftBig')
+        }
+
+        document.querySelector('.menu__elements').style.maxHeight = `calc(100vh - ${searchHeadingHeight}px)`
+
+        document.querySelector('.menu__elements ul li a.my__account img').setAttribute('src', '../images/icons/person-white.png')
+        document.querySelector('.menu__elements ul li a.my__cart img').setAttribute('src', '../images/icons/cart-white.png')
+    })
+}
+
+if (window.innerWidth < 992) {
+    mobileFunctionality()
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 992) {
+        mobileFunctionality()
+    }
+})
